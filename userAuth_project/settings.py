c92ccfa917485a733a28602e99ca2594e457d667
chionz <chideraprince9@gmail.com>
2024-07-08 16:50:42 +0100

    bash deleted

--- userAuth_project/settings.py
+++ userAuth_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'userAuth_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
