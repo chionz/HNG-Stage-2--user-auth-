b4d961bad9aef19a2ed816e93e7969d34846d267
chionz <chideraprince9@gmail.com>
2024-07-08 16:04:00 +0100

    superbase

--- userAuth_project/settings.py
+++ userAuth_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+-(nwbhdobrbdv139l%cm9vfq6(6q3g%ukciwv_r@%=yj%bvi2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','127.0.0.1']
 
 
 # Application definition
@@ -84,20 +84,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'userAuth_project.wsgi.application'
 
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase', 
-        'USER': 'odera',
-        'PASSWORD': '792556aA*',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}"""
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
@@ -105,17 +94,6 @@ DATABASES = {
         'PORT': os.environ.get("DB_PORT"),
     }
 }
-
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 
 # Password validation
